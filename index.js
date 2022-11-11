df74dd28a12a21a9aa5747207db806b3b1067276
RodrigoContreras <contrerasrubiorodrigo@gmail.com>
2022-11-11 13:34:10 -0300

    se agrega querySelector en la funcion createTask para guardar los valores del evento en la variable createTask

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const boton = querySelector(".task").contentText
+const btn = document.querySelector("[data-form-btn]");
+
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    console.log(input.value);
+}
+
+btn.addEventListener("click", (createTask))
