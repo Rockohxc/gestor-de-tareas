abcf5648b2dee101fbfc87ffd90bef26ad39dbed
RodrigoContreras <contrerasrubiorodrigo@gmail.com>
2022-11-12 00:05:15 -0300

    Terminada funcionalidad de agregar tareas y darle check a icono exportado de font awesome

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
+( () => {
+
 const btn = document.querySelector("[data-form-btn]");
-
-// creación de la función anonima que recibe evento como parametro
 const createTask = (evento) => {
-
-    // preventDefault sirve para evitar la recarga de la pag. cuando se activa un evento
     evento.preventDefault();
-
-    // Guardamos en input todo lo que esta en la casilla de la etiqueta input
     const input = document.querySelector("[data-form-input]");
-
-    // guardamos en value el resultado de input
     const value = input.value;
-
-    // 
     const list = document.querySelector("[data-list]");
-
-    // task sirve para crear el valor de la <li>
     const task = document.createElement("li");
-
     task.classList.add("card");
-
-    //Una vez guardamos el valor en value, reiniciamos el input
     input.value = "";
 
-    //Creamos un template y le incluimos el valor de value dentro del template
-    let content = `<div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-        </div>
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    const content = `
         <i class="fas fa-trash-alt trashIcon icon"></i>`;
-
-    // innerHTML sirve para reemplazar el HTML, hay que tener cuuidado porque reemplaza hasta las etiquetas que tenga el ID o el elemento
-    task.innerHTML = content;
-
+    // task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
-
-    console.log(content);
 }
+btn.addEventListener("click", (createTask));
 
-btn.addEventListener("click", (createTask))
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", (completeTask));
+    return i;
+};
+
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+};
+})();
